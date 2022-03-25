56aef778af67455f7ca4ee0812cea1c2943fc70b
Robyn Lam <robynlamgy@gmail.com>
2022-03-25 15:38:56 -0700

    added score-word post request

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from operator import is_
+from pickle import FALSE
 from flask import Flask, request, render_template, jsonify
 from uuid import uuid4
 
@@ -9,6 +11,7 @@ app.config["SECRET_KEY"] = "this-is-secret"
 # The boggle games created, keyed by game id
 games = {}
 
+#c8dafb35-17f4-42d8-bf5a-92fb764a6063
 
 @app.get("/")
 def homepage():
@@ -26,4 +29,25 @@ def new_game():
     game = BoggleGame()
     games[game_id] = game
 
-    return {"gameId": "need-real-id", "board": "need-real-board"}
+    return {"gameId": game_id, "board": game.board}
+
+@app.post("/api/score-word")
+def score_word():
+    """Score word and return JSON: {result: ...}"""
+
+    response = request.json
+    word_input = response["word-input"]
+    print("word_input..",word_input)
+    game_id = response["gameId"]
+
+    print("response...",response)
+    game_instance = games[game_id]
+    print("game instance...",game_instance)
+
+    if game_instance.is_word_in_word_list(word_input) is False:
+        return {"result":"not-word"}
+    if game_instance.check_word_on_board(word_input) is False:
+        return{"result":"not-on-board"}
+    return {"result":"ok"}
+
+
--- test_app.py
+++ test_app.py
@@ -23,12 +23,26 @@ class BoggleAppTestCase(TestCase):
 
         with self.client as client:
             response = client.get('/')
-            ...
+            html = response.get_data(as_text=True)
             # test that you're getting a template
+            # nice to not include ending tag for more flexibility
+            self.assertIn('<table class="board"',html)
 
     def test_api_new_game(self):
         """Test starting a new game."""
 
         with self.client as client:
-            ...
-            # write a test for this route
+            response = client.post('/api/new-game')
+
+            # is JSON
+            self.assertTrue(response.is_json)
+            # has gameID
+            self.assertIn("gameId",response.get_json())
+            # is game board a list-of-lists
+            gameboard = response.get_json()["board"]
+            game_id = response.get_json()["gameId"]
+            self.assertEqual(type(gameboard),list)
+            self.assertEqual(type(gameboard[0]),list)
+            breakpoint()
+            #the route stores the new game in the games dictionary
+            self.assertIn(game_id,games)
