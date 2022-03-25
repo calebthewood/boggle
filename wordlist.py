b74aa2edd1893525bff040e9f32a0482796362fb
calebthewood <calebwood.cs@gmail.com>
2022-03-25 12:15:32 -0700

    adds check_wordlist function to WordList class

--- wordlist.py
+++ wordlist.py
@@ -28,7 +28,18 @@ class WordList:
         return words
 
     def check_word(self, word):
-        """Is word in word list?"""
+        """Is word in word list?
+
+            >>> wl = WordList("dictionary.txt")
+            >>> wl.check_word("POTATO")
+            True
+
+            >>> wl = WordList("dictionary.txt")
+            >>> wl.check_word("XQLP")
+            False
+        """
+
+
 
         return word in self.words
 
